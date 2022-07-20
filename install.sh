d35b978b1177ea2709206ce8a9ba2a9726162a71
bmsjava <35096159+bmsjava@users.noreply.github.com>
2022-07-20 15:38:52 +0300

    Update install.sh
    Исправил парсинг номера версии для webdriver

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list
 apt-get update
 apt-get install -y google-chrome-stable
-chrome_version=$(google-chrome --version|awk '{split($0,a," "); print a[3]}')
+chrome_version=$(google-chrome --version|awk '{split($0,a," "); print a[3]}'|awk '{split($0,a,"."); print a[1]}')
+html=$(curl -k --silent 'https://chromedriver.chromium.org/downloads')
+version=$(grep -oPm1 "ChromeDriver "$chrome_version"\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+" <<< "$html" | awk '{split($0,a," "); print a[2]}' | head -1)
 wget -N https://chromedriver.storage.googleapis.com/$chrome_version/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
