a4d190e1fff49832636d91f34776b6498e35a9dc
bmsjava <35096159+bmsjava@users.noreply.github.com>
2022-07-20 23:52:44 +0300

    Добавил новые сайты для поиска прокси

--- modules/get_proxy.py
+++ modules/get_proxy.py
@@ -9,13 +9,14 @@ from multiprocessing import Pool
 
 from color_log import color_log, yellow, green
 
-r = requests.get('https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt')
+color_log(f'Запущен поиск прокси ...', yellow)
+url1 = 'https://api.pro'
+r = requests.get(f'{url1}xyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=2250&country=all&simplified=true')
 proxy_list = re.findall(r'\d+\.\d+\.\d+\.\d+:\d+', r.text)
 all_proxy = ['socks5://' + i for i in proxy_list]
-r = requests.get('https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt')
+r = requests.get(f'{url1}xyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=2350&country=all&simplified=true')
 proxy_list = re.findall(r'\d+\.\d+\.\d+\.\d+:\d+', r.text)
-[all_proxy.append('socks5://' + i) for i in proxy_list]
-color_log(f'Запущен поиск прокси ...', yellow)
+[all_proxy.append('socks4://' + i) for i in proxy_list]
 good_proxy = []
 
 def _get_proxy(proxy: str) -> List[str]:
@@ -24,7 +25,7 @@ def _get_proxy(proxy: str) -> List[str]:
         'https':proxy
     }
     try:
-        r = requests.get('https://www.skibbel.com/robots.txt', proxies = proxies, timeout = 3)
+        r = requests.get('https://www.skibbel.com/', proxies = proxies, timeout = 2)
         good_proxy.append(proxy)
     except:
         pass
@@ -32,7 +33,7 @@ def _get_proxy(proxy: str) -> List[str]:
 
 
 def get_good_proxy() -> List[str]:
-    with Pool(150) as p:
+    with Pool(100) as p:
         good_proxy_list = p.map(_get_proxy, all_proxy)
     good_proxy_list = [i1 for i in good_proxy_list for i1 in i if len(i1) != 0]
     color_log(f'Найдено {len(good_proxy_list)} прокси', green)
