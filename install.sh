346bdb63e28dfdd0440eb44b982fdfd133dde69b
bmsjava <35096159+bmsjava@users.noreply.github.com>
2022-07-29 18:30:39 +0300

    Добавил скачивание curl

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list
 apt-get update
 apt-get install -y google-chrome-stable
+sudo apt install curl
 chrome_version=$(google-chrome --version|awk '{split($0,a," "); print a[3]}'|awk '{split($0,a,"."); print a[1]}')
 html=$(curl -k --silent 'https://chromedriver.chromium.org/downloads')
 version=$(grep -oPm1 "ChromeDriver "$chrome_version"\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+" <<< "$html" | awk '{split($0,a," "); print a[2]}' | head -1)
