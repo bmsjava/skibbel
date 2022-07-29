b842e3799714d71b8bd1a6da4f193a24d8188e7b
bmsjava <35096159+bmsjava@users.noreply.github.com>
2022-07-30 02:00:18 +0300

    Добавил очистку ип с открытым потром 80

--- modules/get_proxy.py
+++ modules/get_proxy.py
@@ -4,6 +4,7 @@
 
 import requests
 import re
+import socket
 from typing import List
 from multiprocessing import Pool
 
@@ -11,25 +12,39 @@ from color_log import color_log, yellow, green
 
 color_log(f'Запущен поиск прокси ...', yellow)
 url1 = 'https://api.pro'
-r = requests.get(f'{url1}xyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=3000&country=all&simplified=true&ssl=all&anonymity=elite,anonymous')
+r = requests.get(f'{url1}xyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=3000&country=all&simplified=true')
 proxy_list = re.findall(r'\d+\.\d+\.\d+\.\d+:\d+', r.text)
 all_proxy = ['socks5://' + i for i in proxy_list]
-r = requests.get(f'{url1}xyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=3000&country=all&simplified=true&ssl=all&anonymity=elite,anonymous')
+r = requests.get(f'{url1}xyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=3000&country=all&simplified=true')
 proxy_list = re.findall(r'\d+\.\d+\.\d+\.\d+:\d+', r.text)
 [all_proxy.append('socks4://' + i) for i in proxy_list]
+r = requests.get(f'{url1}xyscrape.com/v2/?request=getproxies&protocol=https&timeout=3000&country=all&simplified=true')
+proxy_list = re.findall(r'\d+\.\d+\.\d+\.\d+:\d+', r.text)
+[all_proxy.append('https://' + i) for i in proxy_list]
 good_proxy = []
 
 def _get_proxy(proxy: str) -> List[str]:
-    proxies = {
-        'http':proxy,
-        'https':proxy
-    }
-    try:
-        r = requests.get('https://www.skibbel.com/', proxies = proxies, timeout = 2)
-        good_proxy.append(proxy)
-    except:
-        pass
-    return good_proxy
+    port = re.search(r':(\d+)', proxy)
+    if port != '80' and port != '8080' and port != '3128' and port != '1080' and port != '8123':
+        proxies = {
+            'http':proxy,
+            'https':proxy
+        }
+        try:
+            requests.get('https://www.skibbel.com/', proxies = proxies, timeout = 2)
+            ip = re.search(r'(\d+\.\d+\.\d+\.\d+):\d+', proxy).group(1)
+            try:
+                sock = socket.socket()
+                sock.settimeout(0.5)
+                sock.connect((ip, 80))
+                sock.close()
+            except:
+                good_proxy.append(proxy)
+            else:
+                pass
+        except:
+            pass
+        return good_proxy
 
 
 def get_good_proxy() -> List[str]:
